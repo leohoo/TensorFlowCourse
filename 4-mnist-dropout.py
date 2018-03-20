202ed69ad358cfdfced8fb61597713015358c29e
Wei Liu <lwsoftare@gmail.com>
2018-03-20 13:40:19 +0900

    tf.train.AdagradOptimizer

--- 4-mnist-dropout.py
+++ 4-mnist-dropout.py
@@ -3,7 +3,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
 
-batch_size = 30
+batch_size = 100
 
 n_batch = mnist.train.num_examples // batch_size
 
@@ -11,32 +11,39 @@ x = tf.placeholder(tf.float32, [None, 784])
 y = tf.placeholder(tf.float32, [None,10])
 keep_prob = tf.placeholder(tf.float32)
 
-M = 500
+M = 700
 W = tf.Variable(tf.truncated_normal([784, M], stddev=0.1))
 b = tf.Variable(tf.truncated_normal([M], stddev=0.1))
 L1 = tf.nn.tanh(tf.matmul(x, W)+b)
 L1_drop = tf.nn.dropout(L1, keep_prob)
 
-N = 500
+N = 450
 W2 = tf.Variable(tf.truncated_normal([M, N], stddev=0.1))
 b2 = tf.Variable(tf.zeros([N]))
 L2 = tf.nn.tanh(tf.matmul(L1_drop, W2)+b2)
 L2_drop = tf.nn.dropout(L2, keep_prob)
 
-P = 200
+P = 150
 W3 = tf.Variable(tf.truncated_normal([N, P], stddev=0.1))
 b3 = tf.Variable(tf.zeros([P]))
 L3 = tf.nn.tanh(tf.matmul(L2_drop, W3)+b3)
 L3_drop = tf.nn.dropout(L3, keep_prob)
 
-W4 = tf.Variable(tf.truncated_normal([P, 10], stddev=0.1))
-b4 = tf.Variable(tf.zeros([10]))
+# Q = 50
+# W4 = tf.Variable(tf.truncated_normal([P, Q], stddev=0.1))
+# b4 = tf.Variable(tf.zeros([Q]))
+# L4 = tf.nn.tanh(tf.matmul(L3_drop, W4)+b4)
+# L4_drop = tf.nn.dropout(L4, keep_prob)
 
-prediction = tf.nn.softmax(tf.matmul(L3_drop, W4)+b4)
+Wo = tf.Variable(tf.truncated_normal([P, 10], stddev=0.1))
+bo = tf.Variable(tf.zeros([10]))
+
+prediction = tf.nn.softmax(tf.matmul(L3_drop, Wo)+bo)
 
 # loss = tf.reduce_mean(tf.square(y-prediction))
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=prediction))
-train_step = tf.train.GradientDescentOptimizer(0.3).minimize(loss)
+# train_step = tf.train.GradientDescentOptimizer(0.3).minimize(loss)
+train_step = tf.train.AdagradOptimizer(0.3).minimize(loss)
 
 init = tf.global_variables_initializer()
 
@@ -46,11 +53,11 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 with tf.Session() as sess:
     sess.run(init)
-    for epoch in range(28):
+    for epoch in range(100):
         for batch in range(n_batch):
             batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-            sess.run(train_step, feed_dict={x:batch_xs, y:batch_ys, keep_prob:0.6})
+            sess.run(train_step, feed_dict={x:batch_xs, y:batch_ys, keep_prob:1})
 
         acc = sess.run(accuracy, feed_dict={x:mnist.test.images, y:mnist.test.labels, keep_prob:1.0})
         train_acc = sess.run(accuracy, feed_dict={x:mnist.train.images, y:mnist.train.labels, keep_prob:1.0})
-        print("Iter " + str(epoch) + ", Testing accuracy " + str(acc) + ", Train acc: " + str(train_acc))
+        print("Iter " + str(epoch) + ", Testing accuracy " + str(acc) + ", Train acc: " + str(train_acc) + ", Delta: " + str(train_acc - acc))
